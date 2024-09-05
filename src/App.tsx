78992ccd7f9f4841073d9968be849da2e6027ed2
RABBIT <abc12345abc123april@gmail.com>
2024-09-06 00:55:18 +0800

    change some of the code for nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import * as React from 'react';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ArchiveIcon from '@mui/icons-material/Archive';
-import Home from "@mui/icons-material/Home"
-import Paper from '@mui/material/Paper';
+import FixedBottomNavigation from "./components/nav";
 
-export default function FixedBottomNavigation() {
-  const [value, setValue] = React.useState(0)
-
+export default function app() {
   return (
     <>
-    <>hello world</>
-      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
-        <BottomNavigation showLabels value={value} 
-          onChange={(_event, newValue) => { setValue(newValue) }}>
-          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-          <BottomNavigationAction label="Home" icon={<Home />} />
-          <BottomNavigationAction label="Archive" icon={<ArchiveIcon />} />
-        </BottomNavigation>
-      </Paper>
+      <>hello world</>
+      <FixedBottomNavigation/>
     </>
   );
 }
